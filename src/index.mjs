9a468ed9c5ad1091c463313f7472ab3b256c631e
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2023-09-27 17:19:48 -0300

    Rewriting src/index.js - PART 2
    The focus here was on making both `initHub` and `initWorker` look less
    of a mess. I cleaned up console logs, code comments and added several
    strategic blank lines to better help visually separate concept blocks.

--- src/index.mjs
+++ src/index.mjs
@@ -12,17 +12,31 @@ import minimist from "minimist";
 import * as utils from "./utils/index.js";
 import * as peers from "./utils/peers/index.js";
 
+/**
+ * Initializes HUB node.
+ */
 const initHub = async () => {
   try {
-    const { publicKey, privateKey, address } = await utils.getKeyPair();
-    console.log(`Starting as Hub, address: '${address}'`);
+    // We use privateKey to sign client token (this token will be used
+    // by worker nodes).
+
+    // At this stage we don't need to share publickey with anyone, it's
+    // only used to generate `address` by hashing its content with
+    // SHA-256. In the future publickey can be shared with `lighthouse`
+    // to prevent impersonation (already implemented.) TODO: if it's
+    // already implemented, why the mention is still here? Check if it's
+    // true.
+
+    // `address` is used to find publickey in the database.
+
+    const { privateKey, publicKey, address } = await utils.getKeyPair();
+
+    console.log("Starting HUB at", address);
+
     const token = await utils.createToken(privateKey, address);
 
-    console.log('#########################################################');
-    console.log('\n Start a worker using the following command:\n');
-    console.log(`npm start -- -k ${token}`);
-    console.log('\n');
-    console.log('#########################################################');
+    console.log("\nStart a worker using the following command:\n");
+    console.log("\nnpm start -- -k %s\n\n", token);
 
     const headers = {
       publickey: Buffer.from(publicKey).toString('base64'),
@@ -31,47 +45,47 @@ const initHub = async () => {
 
     utils.connectSSE(headers, false, (ev) => {
       const { clientid, payload } = ev;
-      if(!peers.peers[clientid]) {
-        const peer = peers.createPeer(clientid, { initiator: false });
-        peer.on('signal', signal => {
-          utils.reply({ clientid, ...headers }, signal);
-        });
-      }
-      // I'm fucking up somewhere and answer gets sent to the hub back
-      if(payload.type !== "answer") peers.peers[clientid].signal(payload);
-    });
 
-    /**
-     * The keys are generated, we use the privateKey to sign the client token (this token will be used by worker nodes).
-     * At this stage we don't need to share the publicKey with anyone, we are just generating the `address` by hashing
-     * the publicKey (SHA-256), in the future we can use the publicKey shared with the `lighthouse` to prevent impersonation(already implemented).
-     * The `address` is to find the publicKey in the db.
-     */
-  }
-  catch(e) {
+      if (!peers.peers[clientid]) {
+        const peer = peers.createPeer(clientid, { initiator: false });
+
+        peer.on("signal", (s) => utils.reply({ clientid, ...headers }, s));
+      }
+
+      // In case of errors, send `payload` back to HUB
+
+      if (payload.type !== "answer") peers.peers[clientid].signal(payload);
+    });
+  } catch (e) {
     console.error(e);
   }
 };
 
-const initWorker = async k => {
+/**
+ * Initializes WORKER node.
+ */
+const initWorker = (apiKey) => {
   try {
     const clientid = utils.generateClientId();
     global._clientid = clientid;
-    console.log(`Starting as Worker, id: '${clientid}'`);
+
+    console.log("Starting WORKER id", clientid, "with KEY", apiKey);
+
     const headers = {
       token: apiKey,
-      clientid
+      clientid,
     };
-    utils.connectSSE(headers, () => {
-      const peer = peers.createPeer('hub', { initiator: true });
-      peer.on('signal', signal => {
-        utils.reply(headers, signal);
+
+    utils.connectSSE(
+      headers,
+      () => {
+        const peer = peers.createPeer("hub", { initiator: true });
+
+        peer.on("signal", (s) => utils.reply(headers, s));
+      }, (ev) => {
+        peers.peers["hub"].signal(ev);
       });
-    }, ev => {
-      peers.peers['hub'].signal(ev);
-    });
-  }
-  catch(e) {
+  } catch (e) {
     console.error(e);
   }
 };
