a4d9cc79801f6df807dd6631f5a122fe9fb6271a
Heinrich van Tol Taver <git.heinrich.taver@runbox.com>
2023-09-30 22:34:29 -0300

    file-share.js rewrite DONE
    Similar to what I made with `peers.js`, this so called `file-share.js`
    refactor is basically applying a general code makeup.
    
    Again I made some adjustments on comments and logs, adjusted some
    variables names, general code cleanup.
    
    This commit ends my `refactor-2309` endeavour, both here and on
    `c1-lighthouse`.
    
    Now comes the talk with the team, and everything going well, the heavy
    work should start immediately.

--- src/file-share.js
+++ src/file-share.js
@@ -9,63 +9,75 @@
  * @module file-share
  */
 
-/**
- * Here we handle the p2p file-sharing, we will start with the star topology beacause mesh can get pretty hard.
- * Steps:
- * - Worker connected
- * - Hub sends all fs metadata:
- *  - mimetype or extension
- *  - size in bytes
- *  - file name
- */
+// Here we handle p2p file-sharing.
+//
+// We will start with star topology beacause mesh can get pretty hard.
+//
+// Steps:
+//
+// - Worker connected
+// - Hub sends all fs metadata:
+//  - mimetype or extension
+//  - size in bytes
+//  - file name
 
 /**
  * Module dependencies.
  */
+
+// runtime
 import fs from 'fs';
 import fsP from 'fs/promises';
 import path from 'path';
 
-const startSync = async (peer) => {
-  if(global._role === 'hub') {
-    const files = await fsP.readdir('./shared/files');
-    files.forEach(fp => createChannelAndSend(fp, peer));
-  }
-
-  if(global._role === 'worker') {
-    // We are using a different folder on the worker so we can test in the same folder (hub and workers)
-    if(!fs.existsSync('./w')) {
-      fs.mkdirSync('./w');
-    }
-    if(!fs.existsSync(`./w/${global._clientid}`)) {
-      fs.mkdirSync(`./w/${global._clientid}`);
-      if(!fs.existsSync(`./w/${global._clientid}/files`)) fs.mkdirSync(`./w/${global._clientid}/files`);
-    }
-    // Here we set the global for the "local file directory"
-    global._localFilePath = path.join(process.cwd(), `./w/${global._clientid}/files`);
-    // global._localFilePath = `./w/${global._clientid}/files`;
-  }
-};
-
+/**
+ * Creates a channel for sending files.
+ */
 const createChannelAndSend = (fileName, peer) => {
-  const ds = peer._pc.createDataChannel(`file_${fileName}`);
-  const file = fs.createReadStream(`./shared/files/${fileName}`);
+  const dc = peer._pc.createDataChannel(`file_${fileName}`);
+  const rstream = fs.createReadStream(`./shared/files/${fileName}`);
+
   console.log(`trying to open datachannel file_${fileName}`);
+
   setTimeout(() => {
-    file.on('data', data => {
+    rstream.on("data", (d) => {
       console.log(`sending chunk (${fileName})`);
-      ds.send(data);
+      dc.send(d);
     });
-    file.on('end', () => {
-      ds.close();
+
+    rstream.on("end", () => {
+      dc.close();
     });
   }, 1000);
 };
 
+/**
+ * Synchronises all the nodes.
+ */
+export const startSync = async (peer) => {
+  if (global._role === "hub") {
+    const files = await fsP.readdir("./shared/files");
 
-const receiveFiles = async () => {};
+    files.forEach((f) => createChannelAndSend(f, peer));
+  }
 
-export {
-  startSync,
-  receiveFiles
+  if (global._role === "worker") {
+    // We are using a different folder on worker so we can test in the
+    // same folder (hub and workers)
+
+    fs.existsSync("./w") || fs.mkdirSync("./w");
+
+    if (!fs.existsSync(`./w/${global._clientid}`)) {
+      fs.mkdirSync(`./w/${global._clientid}`);
+
+      fs.existsSync(`./w/${global._clientid}/files`)
+        || fs.mkdirSync(`./w/${global._clientid}/files`);
+    }
+
+    // Here we set global for "local file directory"
+
+    global._localFilePath = path.join(process.cwd(), `./w/${global._clientid}/files`);
+  }
 };
+
+// export const receiveFiles = () => {};
